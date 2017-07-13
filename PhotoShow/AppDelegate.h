6090eeb5a10243edffcc43acde51e86e442381d1
cheerslvtao <sfc-a@sfc-a.local>
2017-07-13 09:12:36 +0800

    Initial Commit

---
+++ PhotoShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoShow
+//
+//  Created by SFC-a on 2017/7/13.
+//  Copyright © 2017年 lvtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoShow
+//
+//  Created by SFC-a on 2017/7/13.
+//  Copyright © 2017年 lvtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
